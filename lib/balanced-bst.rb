ef526ebe4b209e1dff614e05095c89762ec6fde5
menchx <43462510+menchxCodes@users.noreply.github.com>
2023-03-30 20:45:20 +0330

    Add #build_tree

--- lib/balanced-bst.rb
+++ lib/balanced-bst.rb
@@ -8,30 +8,45 @@ class Node
     @right = right
   end
 
-  def <=>(anOther)
-    self.value <=> anOther.value
+  def <=>(other)
+    value <=> other.value
   end
 end
 
 class Tree
+  attr_accessor :root
+
   def initialize(array)
     @root = build_tree(array.uniq.sort)
   end
 
   def build_tree(array)
     #-TODO-
-    mid = array.length/2
-    array[mid]
+
+    if array.length == 1
+      node = Node.new(array[0])
+      return node
+    end
+
+    mid = array.length / 2
+
+    array_left = array[0..mid - 1]
+    left = build_tree(array_left) unless array_left.empty?
+
+    array_right = array[mid + 1..array.length]
+    right = build_tree(array_right) unless array_right.empty?
+
+    node = Node.new(array[mid], left, right)
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
-
+# --tests--
 test = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+
 tree = Tree.new(test)
-p tree
-p test.uniq.sort
+tree.pretty_print
