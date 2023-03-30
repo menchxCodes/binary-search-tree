6849321f462f9eb93e38fddfd86dd73e813c3a99
menchx <43462510+menchxCodes@users.noreply.github.com>
2023-03-30 17:58:17 +0330

    Create Node & Tree Class

---
+++ lib/balanced-bst.rb
@@ -0,0 +1,37 @@
+class Node
+  include Comparable
+  attr_accessor :value, :left, :right
+
+  def initialize(value = nil, left = nil, right = nil)
+    @value = value
+    @left = left
+    @right = right
+  end
+
+  def <=>(anOther)
+    self.value <=> anOther.value
+  end
+end
+
+class Tree
+  def initialize(array)
+    @root = build_tree(array.uniq.sort)
+  end
+
+  def build_tree(array)
+    #-TODO-
+    mid = array.length/2
+    array[mid]
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+end
+
+test = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+tree = Tree.new(test)
+p tree
+p test.uniq.sort
